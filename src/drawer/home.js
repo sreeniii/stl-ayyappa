1ae646eba75b27333c3ba7085691ede97a94ea78
Sreenivas Kanthamneni <sreenivas.kanthamneni@monsanto.com>
2017-12-17 20:59:59 -0600

    fontello font

--- src/drawer/home.js
+++ src/drawer/home.js
@@ -1,12 +1,81 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, StyleSheet, Platform } from 'react-native';
+import { createIconSetFromFontello } from 'react-native-vector-icons';
+
+import {
+  registerCustomIconType,
+  Text,
+  Button,
+  Icon,
+  SocialIcon,
+  Card
+} from 'react-native-elements';
+
+import colors from 'HSColors';
+import socialColors from 'HSSocialColors';
+import fonts from 'HSFonts';
+import fontelloConfig from '../../assets/fontello/config.json';
+
 
 export default class Home extends React.Component {
     render() {
       return (
-      <View>
-        <Text>Hello!</Text>
-      </View>
+        <ScrollView>
+            <View style={styles.hero}>
+                <Icon color="white" name="whatshot" size={62} type="material" />
+                <Text style={styles.heading}>Home</Text>
+            </View>
+        </ScrollView>
       );
-    }
-  }
+    };
+    navigationOptions = {
+        drawerLabel: 'Home',
+        drawerIcon: ({ tintColor }) => (
+          <Icon
+            name="home"
+            size={30}
+            iconStyle={{
+              width: 50,
+              height: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+            type="material-community"
+            color={tintColor}
+          />
+        ),
+      };
+};
+
+
+
+const styles = StyleSheet.create({
+    heading: {
+      color: 'white',
+      marginTop: 10,
+      fontSize: 22,
+    },
+    hero: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 40,
+      backgroundColor: colors.primary2,
+    },
+    titleContainer: {},
+    button: {
+      marginTop: 15,
+    },
+    title: {
+      textAlign: 'center',
+      color: colors.grey2,
+      ...Platform.select({
+        ios: {
+          fontFamily: fonts.ios.black,
+        },
+      }),
+    },
+    socialRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+  });
